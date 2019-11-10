b4e2910a7d7fd3d3172f7bd71c5e79d1ce608c4d
Pavel Churzin <pchurzin@pchurzin.ru>
2019-11-10 12:32:58 +0300

    substitute EnableWebMvc with our config

--- src/main/java/ru/pchurzin/spring/customannotations/WebConfig.java
+++ src/main/java/ru/pchurzin/spring/customannotations/WebConfig.java
@@ -3,11 +3,16 @@ package ru.pchurzin.spring.customannotations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected RequestMappingHandlerMapping createRequestMappingHandlerMapping() {
+        return new RequestMappingHandlerMapping();
+    }
+
     @Bean
     public TestController getTestController() {
         return new TestController();
